56ac1ae477678fd460973b25b6bd9581d9ecee7e
Jeremiah Douglas <jeremiahdouglas323@yahoo.com>
2026-05-21 20:07:56 -0400

    Configure Django settings for PostgreSQL database

--- university_portal/settings.py
+++ university_portal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'university_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'university_db'),
+        'USER': os.getenv('POSTGRES_USER', 'university_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'university_password'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
