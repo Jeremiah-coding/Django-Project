ef2d59432b7af53335ae5eb2cc6fdc8f64728d6a
Jeremiah Douglas <jeremiahdouglas323@yahoo.com>
2026-05-21 20:20:19 -0400

    Add Student model and admin registration

--- students/admin.py
+++ students/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from . import models
+from .models import Student
 
-if hasattr(models, "Student"):
-	admin.site.register(models.Student)
+admin.site.register(Student)
